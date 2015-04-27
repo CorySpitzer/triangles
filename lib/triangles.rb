aa631d5ab645caaceaa257ba17542cf119cf8037
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 12:03:20 -0700

    Consider shortest and longest sides to pass test for a triad with one side length zero and one of non-zero.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -4,10 +4,13 @@ class Triad
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @sorted_sides = [side1, side2, side3].sort()
   end
 
   define_method(:triangle_test) do
-    true
+    shortest_side = @sorted_sides.at(0)
+    longest_side = @sorted_sides.at(2)
+    not((shortest_side == 0) and (shortest_side != longest_side)) 
   end
 
 end
