1e23b9c96bc267f07cd5938828b1ee2dcbf2168d
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 14:22:18 -0700

    Return equilateral for an equilateral triangle

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,22 +1,23 @@
 class Triad
 
   define_method(:initialize) do |side1,side2,side3|
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
     @sorted_sides = [side1, side2, side3].sort()
+    @shortest_side = @sorted_sides.at(0)
+    @mid_side = @sorted_sides.at(1)
+    @longest_side = @sorted_sides.at(2)
   end
 
   define_method(:triangle?) do
-    shortest_side = @sorted_sides.at(0)
-    mid_side = @sorted_sides.at(1)
-    longest_side = @sorted_sides.at(2)
-    # not((shortest_side == 0) and (shortest_side != longest_side))
-    if shortest_side == longest_side || (shortest_side + mid_side) > longest_side
+      if @shortest_side == @longest_side || (@shortest_side + @mid_side) > @longest_side
       true
     else
       false
     end
   end
 
+  define_method(:triangle_type) do
+    if @shortest_side == @longest_side
+      return "equilateral"
+    end
+  end
 end
