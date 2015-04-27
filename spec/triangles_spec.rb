e777b1d3f39445fe555b29613cd76bb7ce0aa3af
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 11:38:49 -0700

    Capitalize triad

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
 require('triangles')
 
-describe('triad#triangle_test') do
+describe('Triad#triangle_test') do
   it('returns true for a triangle with all sides of length 0') do
-    triangle = triad.new(0,0,0)
+    triangle = Triad.new(0,0,0)
     expect(triangle.triangle_test()).to(eq(true))
   end
 
