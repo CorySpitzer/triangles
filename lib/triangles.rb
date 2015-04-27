0b8f722172a434845aea254294ac28e2a022848b
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 12:09:28 -0700

    Change 'triangle_test' to 'triangle?'

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -7,10 +7,10 @@ class Triad
     @sorted_sides = [side1, side2, side3].sort()
   end
 
-  define_method(:triangle_test) do
+  define_method(:triangle?) do
     shortest_side = @sorted_sides.at(0)
     longest_side = @sorted_sides.at(2)
-    not((shortest_side == 0) and (shortest_side != longest_side)) 
+    not((shortest_side == 0) and (shortest_side != longest_side))
   end
 
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -1,16 +1,18 @@
 require('rspec')
 require('triangles')
 
-describe('Triad#triangle_test') do
+describe('Triad#triangle?') do
   it('returns true for a triangle with all sides of length 0') do
     triangle = Triad.new(0,0,0)
-    expect(triangle.triangle_test()).to(eq(true))
+    expect(triangle.triangle?()).to(eq(true))
   end
 
   it('returns false if one side is zero and any other side is
       not zero') do
     not_triangle = Triad.new(0,0,1)
-    expect(not_triangle.triangle_test()).to(eq(false))
+    expect(not_triangle.triangle?()).to(eq(false))
   end
+  # 
+  # it('')
 
 end
