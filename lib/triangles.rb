a07af8a0559523e8f190fe433b2d634b0eb3c60d
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 14:03:23 -0700

    Check if object is a triangle by considering whether object is equilateral or not a triangle at all because sum of shortest and mid sides is less than longest side

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -9,8 +9,14 @@ class Triad
 
   define_method(:triangle?) do
     shortest_side = @sorted_sides.at(0)
+    mid_side = @sorted_sides.at(1)
     longest_side = @sorted_sides.at(2)
-    not((shortest_side == 0) and (shortest_side != longest_side))
+    # not((shortest_side == 0) and (shortest_side != longest_side))
+    if shortest_side == longest_side || (shortest_side + mid_side) > longest_side
+      true
+    else
+      false
+    end
   end
 
 end
