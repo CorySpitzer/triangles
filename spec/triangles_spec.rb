ed54de7d3267dbc52665b942b3f9be069492e0f1
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 12:18:59 -0700

    Test that if one side is bigger than the other two combined, that it's not a triangle

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -3,16 +3,17 @@ require('triangles')
 
 describe('Triad#triangle?') do
   it('returns true for a triangle with all sides of length 0') do
-    triangle = Triad.new(0,0,0)
-    expect(triangle.triangle?()).to(eq(true))
+    expect(Triad.new(0,0,0).triangle?()).to(eq(true))
   end
 
   it('returns false if one side is zero and any other side is
       not zero') do
-    not_triangle = Triad.new(0,0,1)
-    expect(not_triangle.triangle?()).to(eq(false))
+    expect(Triad.new(0,0,1).triangle?()).to(eq(false))
   end
-  # 
-  # it('')
+
+  it('returns false if the length of one side is greater than
+      the sum of the other two') do
+    expect(Triad.new(1,2,4).triangle?()).to(eq(false))
+  end
 
 end
