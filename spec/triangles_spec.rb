f18747249efce558d3464cd8e45723b9d386190c
cory spitzer & bob ellis <student@epicodus.com>
2015-04-27 14:14:24 -0700

    Improve spec organization and test for equilateral triangles

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -1,19 +1,30 @@
 require('rspec')
 require('triangles')
 
-describe('Triad#triangle?') do
-  it('returns true for a triangle with all sides of length 0') do
-    expect(Triad.new(0,0,0).triangle?()).to(eq(true))
+describe('Triad') do
+  describe('#triangle?') do
+    it('returns true for a triangle with all sides of length 0') do
+      expect(Triad.new(0,0,0).triangle?()).to(eq(true))
+    end
+
+    it('returns false if one side is zero and any other side is
+        not zero') do
+      expect(Triad.new(0,0,1).triangle?()).to(eq(false))
+    end
+
+    it('returns false if the length of one side is greater than
+        the sum of the other two') do
+      expect(Triad.new(1,2,4).triangle?()).to(eq(false))
+    end
+
+    it('returns true if it is a right triangle') do
+      expect(Triad.new(3,4,5).triangle?()).to(eq(true))
+    end
   end
 
-  it('returns false if one side is zero and any other side is
-      not zero') do
-    expect(Triad.new(0,0,1).triangle?()).to(eq(false))
+  describe('#triangle_type') do
+    it('returns equilateral for a triangle with all sides equal') do
+      expect(Triad.new(1,1,1)).triangle_type().to(eq('equilateral'))
+    end
   end
-
-  it('returns false if the length of one side is greater than
-      the sum of the other two') do
-    expect(Triad.new(1,2,4).triangle?()).to(eq(false))
-  end
-
 end
